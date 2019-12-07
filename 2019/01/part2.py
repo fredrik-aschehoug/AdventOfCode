e0b200ebaec4e0b47ca9dae24b49b94df7c6518c
fredrik-aschehoug <15358786+fredrik-aschehoug@users.noreply.github.com>
2019-12-08 00:06:30 +0100

    day 1 part 2 + refactoring

--- 2019/01/part2.py
+++ 2019/01/part2.py
@@ -1,12 +1,19 @@
 from common import loadFile, fileToList, calculateFuel
+import math
 
+
+def calculateFuel2(mass):
+    fuel = math.floor(mass / 3) - 2
+    if fuel <= 0:
+      return 0
+    return fuel + calculateFuel2(fuel)
 
 def main():
   inputHandle = loadFile("01/input.txt")
   massList = fileToList(inputHandle, "\n")
   fuelList = list()
 
-  totalFuel = sum([calculateFuel(mass) for mass in massList])
+  totalFuel = sum([calculateFuel2(mass) for mass in massList])
   
   print(totalFuel)
 
