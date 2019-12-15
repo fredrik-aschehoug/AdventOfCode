0282fec46b9cbc87fe8f5ca45dc2a710aec00e6b
fredrik-aschehoug <15358786+fredrik-aschehoug@users.noreply.github.com>
2019-12-15 16:10:37 +0100

    More day 5 work

--- 2019/05/intcode.py
+++ 2019/05/intcode.py
@@ -1,69 +1,93 @@
 
-    
-def op1(pos1, pos2, pos3, arr):
-  # Add
-  arr[pos3] = arr[pos1] + arr[pos2]
 
-def op2(pos1, pos2, pos3, arr):
-  # Multiply
-  arr[pos3] = arr[pos1] * arr[pos2]
+def op1(arg1, arg2, resPos, arr, mode1, mode2):
+    # Add
+    if mode1 and mode2:
+        arr[resPos] = arg1 + arg2
+    elif mode1:
+        arr[resPos] = arg1 + arr[arg2]
+    elif mode2:
+        arr[resPos] = arr[arg1] + arg2
+    else:
+        arr[resPos] = arr[arg1] + arr[arg2]
+
+
+def op2(arg1, arg2, resPos, arr, mode1, mode2):
+    # Multiply
+    if mode1 and mode2:
+        arr[resPos] = arg1 * arg2
+    elif mode1:
+        arr[resPos] = arg1 * arr[arg2]
+    elif mode2:
+        arr[resPos] = arr[arg1] * arg2
+    else:
+        arr[resPos] = arr[arg1] * arr[arg2]
+
 
 def op3(val, pos, arr):
-  # Write input to position
-  arr[pos] = val
+    # Write input to position
+    arr[pos] = val
+
 
 def op4(pos, arr):
-  # Output value in position
-  print(arr[pos])
+    # Output value in position
+    print(arr[pos])
+
 
 def parseInstruction(instruction):
-  parsed = {
-    "m1": 0,
-    "m2": 0,
-    "m3": 0,
-    "m4": 0
-  }
-  instructionList = list(instruction)
-  instructionLenght = len(instructionList)
+    parsed = {
+        "m1": 0,
+        "m2": 0,
+        "m3": 0,
+        "m4": 0
+    }
+    instructionList = list(instruction)
+    instructionLenght = len(instructionList)
 
-  if instructionLenght == 1:
-    parsed.opCode = int(instruction)
+    if instructionLenght == 1:
+        parsed["opCode"] = int(instruction)
+        return parsed
+
+    parsed["opCode"] = int(instructionList[-1])
+    # Remove last two digits and reverse
+    modes = instructionList[:-2][::-1]
+
+    print(instructionList[:-2])
+    print(modes)
+    print("---------")
+    for i, mode in enumerate(modes):
+        parsed["m{}".format(i)] = mode
+
     return parsed
-  
-  parsed.opCode = int(instructionList[-1])
-  modes = instructionList[:-2].reverse()
 
-  for i, mode in enumerate(modes):
-    parsed["m{}".format(i)] = mode
-  
-  return parsed
 
 def run(inputValue, data):
-  program = data.copy()
-  pos = 0
-  instruction = parseInstruction(program[0])
-  output = None
+    program = data.copy()
+    pos = 0
 
-  while True:
-    if program[pos] == 99:
-      break
-    if program[pos] == 1:
-      operation = op1
-      posIncrement = 4
-    elif program[pos] == 2:
-      operation = op2
-      posIncrement = 4
-    elif program[pos] == 3:
-      operation = op3
-      posIncrement = 2
-    elif program[pos] == 4:
-      operation = op4
-      posIncrement = 2
-    else:
-      print("error, unknown operation")
-      print(program[pos])
-      break
-    
-    operation(program[pos + 1], program[pos + 2], program[pos + 3], program)
-    pos += posIncrement
-  return output
+    while True:
+        instruction = parseInstruction(str(program[pos]))
+        if instruction["opCode"] == 99:
+            break
+        if instruction["opCode"] == 1:
+            print("m1: {}, m2: {}".format(
+                instruction["m1"], instruction["m2"]))
+            op1(program[pos + 1], program[pos + 2],
+                program[pos + 3], program, instruction["m1"], instruction["m2"])
+            posIncrement = 4
+        elif instruction["opCode"] == 2:
+            op2(program[pos + 1], program[pos + 2],
+                program[pos + 3], program, instruction["m1"], instruction["m2"])
+            posIncrement = 4
+        elif instruction["opCode"] == 3:
+            op3(inputValue, program[pos + 1], program)
+            posIncrement = 2
+        elif instruction["opCode"] == 4:
+            op4(program[pos + 1], program)
+            posIncrement = 2
+        else:
+            print("error, unknown operation")
+            print(program[pos])
+            break
+
+        pos += posIncrement
--- 2019/05/part1.py
+++ 2019/05/part1.py
@@ -1,15 +1,18 @@
 import intcode
 from common import *
 
+
 def main():
-  inputVal = 1
-  inputHandle = loadFile("05/input.txt")
-  program = fileToList(inputHandle, ",")
+    inputVal = 1
+    inputHandle = loadFile("05/input.txt")
+    program = fileToList(inputHandle, ",")
 
-  output = intcode.run(program, inputVal)
+    # output = intcode.run(program, inputVal)
+    intcode.run(inputVal, program)
 
-  fout = open('05/output.txt', 'w')
-  fout.write(str(output))
+    # fout = open('05/output.txt', 'w')
+    # fout.write(str(output))
+
 
 if __name__ == "__main__":
     main()
