540f4d77cdcefaafaf294be456d3eb75736ce5ae
fredrik-aschehoug <15358786+fredrik-aschehoug@users.noreply.github.com>
2019-12-23 22:45:50 +0100

    Day 6 part 1 - not optimized

---
+++ 2019/06/common.py
@@ -0,0 +1,27 @@
+def loadFile(fileName):
+    """Return file handle if file exists. 
+    In case of exception, return None"""
+    try:
+        fileHandle = open(fileName, encoding="UTF-8")
+        fileValid = True
+    except:
+        fileValid = False
+    if fileValid:
+        return fileHandle
+    else:
+        return None
+
+
+def fileToList(fh, delimiter):
+    content = fh.read()
+    return content.split(delimiter)
+
+
+def fileToIntList(fh, delimiter):
+    content = fh.read()
+    contentList = content.split(delimiter)
+    return list(map(int, contentList))
+
+
+def listToString(l):
+    return ''.join(map(str, l))
