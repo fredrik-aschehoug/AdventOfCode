accf35ce4dcbf09a76ae710373cfc8f0e39007ac
Fredrik Evensen Aschehoug <15358786+fredrik-aschehoug@users.noreply.github.com>
2020-12-12 01:46:27 +0100

    Day1 done

---
+++ 2020/Day1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = File.ReadLines("input.txt")
+                .Select(line => int.Parse(line))
+                .ToArray();
+            var query = numbers.SelectMany((value, index) => numbers.Skip(index + 1),
+                               (first, second) => new { first, second });
+            foreach (var number in query)
+            {
+                int result = number.first + number.second;
+                if (result == 2020)
+                {
+                    Console.WriteLine(number.first * number.second);
+                }
+            }
+        }
+    }
+}
