17835773ddb2fb6f1b70b414974c0f94f9ee4920
fredrik-aschehoug <15358786+fredrik-aschehoug@users.noreply.github.com>
2020-12-12 15:47:33 +0100

    Day1 part2

--- 2020/Day1/Program.cs
+++ 2020/Day1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -6,11 +7,16 @@ namespace Day1
 {
     class Program
     {
-        static void Main(string[] args)
+        static int[] GetInput()
         {
             int[] numbers = File.ReadLines("input.txt")
                 .Select(line => int.Parse(line))
                 .ToArray();
+            return numbers;
+        }
+        static void Part1()
+        {
+            int[] numbers = GetInput();
             var query = numbers.SelectMany((value, index) => numbers.Skip(index + 1),
                                (first, second) => new { first, second });
             foreach (var number in query)
@@ -21,6 +27,46 @@ namespace Day1
                     Console.WriteLine(number.first * number.second);
                 }
             }
+        }
+        static List<Triplet> GetTriplets(int[] numbers)
+        {
+            List<Triplet> combinations = new List<Triplet>();
+            foreach (int number1 in numbers)
+            {
+                foreach (int number2 in numbers)
+                {
+                    foreach (int number3 in numbers)
+                    {
+                        Triplet combination = new Triplet()
+                        {
+                            first = number1,
+                            second = number2,
+                            third = number3
+                        };
+                        combinations.Add(combination);
+                    }
+
+                }
+
+            }
+            return combinations;
+        }
+        static void Part2()
+        {
+            int[] numbers = GetInput();
+            var combinations = GetTriplets(numbers);
+            foreach (Triplet combination in combinations)
+            {
+                int result = combination.first + combination.second + combination.third;
+                if (result == 2020)
+                {
+                    Console.WriteLine(combination.first * combination.second * combination.third);
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Part2();
         }
     }
 }
---
+++ 2020/Day1/Triplet.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day1
+{
+    public class Triplet
+    {
+        public int first;
+        public int second;
+        public int third;
+
+        //public Triplet(int first, int second, int third)
+        //{
+        //    this.first = first;
+        //    this.second = second;
+        //    this.third = third;
+        //}
+    }
+}
