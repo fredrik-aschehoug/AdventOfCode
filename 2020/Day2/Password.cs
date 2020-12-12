f426af32d97329c6b8fc5d13e22353bd01f44fcb
fredrik-aschehoug <15358786+fredrik-aschehoug@users.noreply.github.com>
2020-12-12 18:25:30 +0100

    Day2 part1 done

---
+++ 2020/Day2/Password.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Day2
+{
+    class Password
+    {
+        public char letter;
+        public int min;
+        public int max;
+        public string password;
+
+        public Password(string password)
+        {
+            string[] slice1 = password.Split("-");
+            string[] slice2 = slice1.Last().Split(" ", 2);
+            string[] slice3 = slice2.Last().Split(": ", 2);
+
+            this.min = int.Parse(slice1.First());
+            this.max = int.Parse(slice2.First());
+            this.letter = char.Parse(slice3.First());
+            this.password = slice3.Last();
+        }
+
+        public bool isValid()
+        {
+            bool validPassword = false;
+            int count = password.Count(c => c == letter);
+            if(count >= min & count <= max)
+            {
+                validPassword = true;
+            }
+            return validPassword;
+        }
+    }
+}
--- 2020/Day2/Program.cs
+++ 2020/Day2/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace Day2
 {
     class Program
     {
+        static string[] GetInput()
+        {
+            string[] lines = File.ReadLines("input.txt")
+                .Select(line => line)
+                .ToArray();
+            return lines;
+        }
+        static void Part1()
+        {
+            var lines = GetInput();
+            List<Password> passwordList = new List<Password>();
+            foreach(string line in lines)
+            {
+                passwordList.Add(new Password(line));
+            }
+            var validPasswords = passwordList.Where(password => password.isValid());
+            Console.WriteLine(validPasswords.Count());
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Part1();
         }
     }
 }
