014786d34da5937cef8d1385f8fc45199bbef17d
fredrik-aschehoug <15358786+fredrik-aschehoug@users.noreply.github.com>
2020-12-24 15:30:23 +0100

    Day5 part1

---
+++ 2020/Day5/BoardingPass.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day5
+{
+    class BoardingPass
+    {
+        private readonly int rowId;
+        private readonly int columnId;
+        private const int maxRow = 127;
+        private const int maxColumn = 7;
+
+        public BoardingPass(List<char> letters)
+        {
+            rowId = GetId(letters.GetRange(0,7), maxRow, 'F');
+            columnId = GetId(letters.GetRange(7, 3), maxColumn, 'L');
+        }
+
+        private int GetColumnId(List<char> letters)
+        {
+            int max = maxColumn;
+            int min = 0;
+            int mid = 0;
+            foreach (var letter in letters)
+            {
+                mid = (min + max) / 2;
+                if (letter == 'L')
+                {
+                    max = mid - 1;
+                }
+                else
+                {
+                    min = mid + 1;
+                }
+            }
+            return mid;
+        }
+
+        private int GetId(List<char> letters, int maxIndex, char lowLetter)
+        {
+            int max = maxIndex;
+            int min = 0;
+            int mid = 0;
+            for (int i = 0; i < letters.Count; i++)
+            {
+                char letter = letters[i];
+                bool isLast = i == letters.Count - 1;
+                mid = (min + max) / 2;
+                if (letter == lowLetter)
+                {
+                    max = mid;
+                    if (isLast) return max;
+                }
+                else
+                {
+                    min = mid + 1;
+                    if (isLast) return min;
+                }
+            }
+            return mid;
+        }
+
+        public int GetSeatId()
+        {
+            int id = rowId * 8 + columnId;
+            return id;
+        }
+    }
+}
---
+++ 2020/Day5/Solver.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day5
+{
+    class Solver
+    {
+        private const string inputPath = "input.txt";
+        private static List<List<char>> GetInput()
+        {
+            var lines = File.ReadLines(inputPath)
+                .Select(line => line.Select(c => c).ToList())
+                .ToList();
+
+            return lines;
+        }
+        public void Part1()
+        {
+            var input = GetInput();
+            List<int> ids = new List<int>();
+
+            foreach (var line in input)
+            {
+                ids.Add(new BoardingPass(line).GetSeatId());
+            }
+            int highest = ids.Max();
+            Console.WriteLine("Part 1: {0}", highest);
+        }
+    }
+}
