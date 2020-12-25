1eb10759fbda15db8f7287c23d62d58b8e1b675e
fredrik-aschehoug <15358786+fredrik-aschehoug@users.noreply.github.com>
2020-12-25 16:30:16 +0100

    Day6 Part1

---
+++ 2020/Day6/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Day6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solver solver = new Solver();
+            solver.Part1();
+        }
+    }
+}
---
+++ 2020/Day6/Solver.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Day6
+{
+    class Solver
+    {
+        public Solver()
+        {
+            lines = GetInput();
+        }
+        private readonly List<List<char>> lines;
+        private const string inputPath = "input.txt";
+        private static List<List<char>> GetInput()
+        {
+            var lines = File.ReadAllText(inputPath)
+                .Split(new string[] { Environment.NewLine + Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(part => part.Replace(Environment.NewLine, ""))
+                .Select(line => line.Select(c => c).ToList()).ToList();
+
+            return lines;
+
+        }
+        
+        public void Part1()
+        {
+            var uniqueAnswers = lines.Select(line => line.Distinct().ToList());
+            int sum = uniqueAnswers.Aggregate(0, (acc, next) => acc + next.Count());
+            Console.WriteLine("Part 1: {0}", sum);
+        }
+    }
+}
