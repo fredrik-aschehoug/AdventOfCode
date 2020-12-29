2c96cbed9715a002ae20cadc6a874e584bc8d833
fredrik-aschehoug <15358786+fredrik-aschehoug@users.noreply.github.com>
2020-12-29 22:13:34 +0100

    Day8 Part1

---
+++ 2020/Day8/Interpreter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day8
+{
+    class Interpreter
+    {
+        private List<(string, int)> _instructions;
+        private int accumulator = 0;
+        private List<int> visited = new List<int>();
+        public Interpreter(List<(string, int)> instructions)
+        {
+            _instructions = instructions;
+        }
+        public int Run()
+        {
+            int currentIndex = 0;
+            while (true)
+            {
+                var instruction = _instructions[currentIndex];
+                int accumulatorIncrement = 0;
+                int indexIncerment;
+                switch (instruction.Item1)
+                {
+                    case "acc":
+                        accumulatorIncrement += instruction.Item2;
+                        indexIncerment = 1;
+                        break;
+                    case "jmp":
+                        indexIncerment = instruction.Item2;
+                        break;
+                    default:
+                        indexIncerment = 1;
+                        break;
+                }
+                int newIndex = currentIndex + indexIncerment;
+                if (visited.Contains(newIndex))
+                {
+                    return accumulator;
+
+                }
+                visited.Add(currentIndex);
+                currentIndex = newIndex;
+                accumulator += accumulatorIncrement;
+            }
+        }
+    }
+}
---
+++ 2020/Day8/Solver.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day8
+{
+    class Solver
+    {
+        public Solver()
+        {
+            instructions = GetInput();
+        }
+        private readonly List<(string, int)> instructions;
+        private const string inputPath = "input.txt";
+        private static List<(string, int)> GetInput()
+        {
+            var lines = File.ReadLines(inputPath)
+                .Select(line => line.Split(" "))
+                .Select(line => (line[0], int.Parse(line[1])))
+                .ToList();
+
+            return lines;
+
+        }
+        public void Part1()
+        {
+            Interpreter computer = new Interpreter(instructions);
+
+            Console.WriteLine("Part 1: {0}", computer.Run());
+        }
+        public void Part2()
+        {
+            Console.WriteLine("Part 2: {0}", 0);
+        }
+    }
+}
