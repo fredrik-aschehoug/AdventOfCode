ad48e7f6d9a8a9c8ed05c3e9d659ea19e9625cd3
fredrik-aschehoug <15358786+fredrik-aschehoug@users.noreply.github.com>
2021-01-10 22:57:06 +0100

    Refactor

---
+++ 2020/Day11/SeatLayout1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day11
+{
+    class SeatLayout1 : SeatLayoutBase
+    {
+        private static int GetOccupiedNeighbors(char[,] map, int row, int column)
+        {
+            int occupiedNeighbors = 0;
+            var rowBounds = Enumerable.Range(map.GetLowerBound(0), map.GetUpperBound(0) + 1);
+            var columnBounds = Enumerable.Range(map.GetLowerBound(1), map.GetUpperBound(1) + 1);
+            var neighborIndices = new List<(int, int)>
+            {
+                (row - 1, column - 1), (row - 1, column), (row - 1, column + 1),
+                (row, column - 1), (row, column + 1),
+                (row + 1, column - 1), (row + 1, column), (row + 1, column + 1)
+            };
+            foreach (var (r, c) in neighborIndices)
+            {
+                if (rowBounds.Contains(r) && columnBounds.Contains(c) && map[r, c] == '#')
+                {
+                    occupiedNeighbors++;
+                }
+            }
+            return occupiedNeighbors;
+        }
+        public override bool ShouldBeOccupied(char[,] map, int row, int column)
+        {
+            return GetOccupiedNeighbors(map, row, column) == 0;
+        }
+        public override bool ShouldBeVacant(char[,] map, int row, int column)
+        {
+            return GetOccupiedNeighbors(map, row, column) >= 4;
+        }
+    }
+}
---
+++ 2020/Day11/SeatLayoutBase.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Day11
+{
+    abstract class SeatLayoutBase
+    {
+        public char[,] SeatMap { get; set; }
+        public char[,] Iterate()
+        {
+            var newMap = new char[SeatMap.GetLength(0), SeatMap.GetLength(1)];
+            for (int rowIndex = 0; rowIndex < SeatMap.GetLength(0); rowIndex++)
+            {
+                for (int columnIndex = 0; columnIndex < SeatMap.GetLength(1); columnIndex++)
+                {
+                    var newValue = SeatMap[rowIndex, columnIndex] switch
+                    {
+                        'L' => ShouldBeOccupied(SeatMap, rowIndex, columnIndex) ? '#' : 'L',
+                        '#' => ShouldBeVacant(SeatMap, rowIndex, columnIndex) ? 'L' : '#',
+                        '.' => '.'
+                    };
+                    newMap[rowIndex, columnIndex] = newValue;
+                }
+            }
+            return newMap;
+        }
+
+        public abstract bool ShouldBeVacant(char[,] oldMap, int rowIndex, int columnIndex);
+
+        public abstract bool ShouldBeOccupied(char[,] oldMap, int rowIndex, int columnIndex);
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType()) return false;
+
+            for (int rowIndex = 0; rowIndex < SeatMap.GetLength(0); rowIndex++)
+            {
+                for (int columnIndex = 0; columnIndex < SeatMap.GetLength(1); columnIndex++)
+                {
+                    if (SeatMap[rowIndex, columnIndex] != ((SeatLayoutBase)obj).SeatMap[rowIndex, columnIndex])
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+        public  int CountOccupied()
+        {
+            return SeatMap.Cast<char>()
+                .Where(seat => seat == '#')
+                .Count();
+        }
+    }
+}
