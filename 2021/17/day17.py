8cfcee351ee1494e73683fd7eca498ffbb2b55d5
fredrik-aschehoug <15358786+fredrik-aschehoug@users.noreply.github.com>
2021-12-23 18:36:15 +0100

    Day 17 part 2

--- 2021/17/day17.py
+++ 2021/17/day17.py
@@ -1,14 +1,11 @@
 
 
-def simulate_launch(x, y, target):
+def simulate_launch(x, y, x_target, y_target, target):
     x_pos = 0
     x_increment = x
     y_pos = 0
     y_increment = y
-    y_target = range(target["y"]["min"], target["y"]["max"])
-    x_target = range(target["x"]["min"], target["x"]["max"])
 
-    velocities = list()
     for i in range(1000):
         if x_increment > 0:
             x_pos += x_increment
@@ -16,10 +13,10 @@ def simulate_launch(x, y, target):
         y_pos += y_increment
         y_increment -= 1
         if x_pos in x_target and y_pos in y_target:
-            velocities.append((x, y))
+            return (x, y)
         if x_pos > target["x"]["max"] and y_pos > target["y"]["min"]:
             break
-    return velocities
+    return None
 
 
 def get_max_y(velocity):
@@ -49,14 +46,19 @@ def main():
 
     velocities = list()
 
-    for x in range(15, 19):
+    y_target = range(target["y"]["min"], target["y"]["max"] + 1)
+    x_target = range(target["x"]["min"], target["x"]["max"] + 1)
+
+    for x in range(500):
         for y in range(target["y"]["min"], 200):
-            current_velocities = simulate_launch(x, y, target)
-            velocities.extend(current_velocities)
+            current_velocity = simulate_launch(x, y, x_target, y_target, target)
+            if current_velocity:
+                velocities.append(current_velocity)
 
     velocities_with_max_y = [get_max_y(velocity) for velocity in velocities]
     max_y = max(velocities_with_max_y)
-    print(max_y)
+    print("part 1: ", max_y)
+    print("part 2: ", len(velocities))
 
 
 if __name__ == "__main__":
