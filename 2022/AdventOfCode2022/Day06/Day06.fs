755a287d1843e671215ab4ef00f27f123501d834
fredrik-aschehoug <15358786+fredrik-aschehoug@users.noreply.github.com>
2022-12-06 14:20:37 +0100

    Day 6

---
+++ 2022/AdventOfCode2022/Day06/Day06.fs
@@ -0,0 +1,15 @@
+﻿module Day06
+
+let isDistinct (text: char[]) =
+    let distinct = Array.distinct text
+    distinct.Length = text.Length
+
+let rec getPacketStart (buffer: string, i: int, lenght: int) =
+    let section = buffer.[i..(i + lenght)].ToCharArray()
+    if (isDistinct section) then i + lenght + 1
+    else getPacketStart (buffer, i + 1, lenght)
+
+let part1 (buffer: string) = getPacketStart (buffer, 0, 3)
+
+let part2 (buffer: string) = getPacketStart (buffer, 0, 13)
+
