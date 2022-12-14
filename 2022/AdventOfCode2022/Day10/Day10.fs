cde4a762369ed4dcdd42ec7bde9ca91bb0900cf2
fredrik-aschehoug <15358786+fredrik-aschehoug@users.noreply.github.com>
2022-12-14 20:15:34 +0100

    Day10 part 1

---
+++ 2022/AdventOfCode2022/Day10/Day10.fs
@@ -0,0 +1,52 @@
+﻿module Day10
+
+open System
+open System.Linq
+open System.Collections.Generic
+
+let cycles = [20; 60; 100; 140; 180; 220;]
+
+type CommandType =
+    | NoOp = 1
+    | AddX = 2
+
+type Command = { Type: CommandType; Argument: int Option}
+
+type Result = { X: int; Clock: int }
+
+let parseCommand (line: string) =
+    let parts = line.Split(" ")
+
+    match parts.[0] with
+    | "noop" -> { Type = CommandType.NoOp; Argument = None }
+    | "addx" -> { Type = CommandType.AddX; Argument = Some(Int32.Parse parts.[1]) }
+
+let getResult (command: Command, previous: Result) =
+    match command.Type with
+    | CommandType.NoOp -> { X = previous.X; Clock = previous.Clock + 1}
+    | CommandType.AddX -> { X = previous.X + command.Argument.Value; Clock = previous.Clock + 2}
+
+let runCommands (commands: Command list) =
+    let firstResult = getResult (commands.Head, { X = 1; Clock = 1 })
+    let results = new List<Result>()
+    results.Add(firstResult)
+    
+    for i = 1 to commands.Length - 1 do
+        let prev = results.Item (i - 1)
+        let current = commands.Item i
+        let result = getResult (current, prev)
+        results.Add(result)
+    
+    results |> List.ofSeq
+
+let rec getRegisterInCycle (cycle: int, results: Result list) =
+    let result = results.Where(fun result -> result.Clock = cycle)
+    if result.Count() <> 0 then result.First().X
+    else getRegisterInCycle (cycle - 1, results)
+
+let getSum (results: Result list) =
+    [for cycle in cycles -> getRegisterInCycle(cycle, results) * cycle] |> List.sum
+
+
+let part1 (lines: string[]) =
+    lines |> Array.toList |> List.map parseCommand |> runCommands |> getSum
--- 2022/AdventOfCode2022/Tests/Tests.fs
+++ 2022/AdventOfCode2022/Tests/Tests.fs
@@ -6,7 +6,7 @@ open Xunit
 [<Theory>]
 [<InlineData("Input/input-day1-test.txt", 24000, 45000)>]
 [<InlineData("Input/input-day1.txt", 68775, 202585)>]
-let ``Day1`` (fileName, part1Result, part2Result) =
+let ``Day01`` (fileName, part1Result, part2Result) =
     let text = File.ReadAllText(fileName)
 
     let topElf = Day01.part1 text
@@ -18,7 +18,7 @@ let ``Day1`` (fileName, part1Result, part2Result) =
 [<Theory>]
 [<InlineData("Input/input-day2-test.txt", 15, 12)>]
 [<InlineData("Input/input-day2.txt", 11386, 13600)>]
-let ``Day2`` (fileName, part1Result, part2Result) =
+let ``Day02`` (fileName, part1Result, part2Result) =
     let lines = File.ReadAllLines(fileName)
 
     let score = Day02.part1 lines
@@ -30,7 +30,7 @@ let ``Day2`` (fileName, part1Result, part2Result) =
 [<Theory>]
 [<InlineData("Input/input-day3-test.txt", 157, 70)>]
 [<InlineData("Input/input-day3.txt", 7872, 2497)>]
-let ``Day3`` (fileName, part1Result, part2Result) =
+let ``Day03`` (fileName, part1Result, part2Result) =
     let lines = File.ReadAllLines(fileName)
     
     let prioritySum = Day03.part1 lines
@@ -42,7 +42,7 @@ let ``Day3`` (fileName, part1Result, part2Result) =
 [<Theory>]
 [<InlineData("Input/input-day4-test.txt", 2, 4)>]
 [<InlineData("Input/input-day4.txt", 431, 823)>]
-let ``Day4`` (fileName, part1Result, part2Result) =
+let ``Day04`` (fileName, part1Result, part2Result) =
     let lines = File.ReadAllLines(fileName)
         
     let strictOverlappingPairs = Day04.part1 lines
@@ -54,7 +54,7 @@ let ``Day4`` (fileName, part1Result, part2Result) =
 [<Theory>]
 [<InlineData("Input/input-day5-test.txt", "CMZ", "MCD")>]
 [<InlineData("Input/input-day5.txt", "TLNGFGMFN", "FGLQJCMBD")>]
-let ``Day5`` (fileName, part1Result, part2Result) =
+let ``Day05`` (fileName, part1Result, part2Result) =
     let text = File.ReadAllText(fileName)
         
     let crates = Day05.part1 text
@@ -73,7 +73,7 @@ let ``Day5`` (fileName, part1Result, part2Result) =
     "vldvlvvhwhttcsttpntnvvqpqpddmlddwhwcwnwmwfwppdrrhllcwwgwhwvhwhshchshtsstzstztgtlggcwwtptrppfpggwpwmppnfpfnfznffmvfmvffgfwwwgrrqgglrgrqqlslhhgjgwwdswsbwwqswqwnqwqpqttqstqtjqqctcbtcbttcptpbbsmmhggwmggjllcpcvcrrphpjjwzwgzgtzggscggwdgghlhnhddlclrcchllrlbbnlbbgpbgghvggpbgpgssbszzjbbcpbpttwztwwgngnqqgllcvlvlzvzwwzssbppjwwvswwbjwbjwwwsjsdjsszmssfvsvcscqssnbnzbzzhsszttfvvdllzjzsjjzzvzsvzzdvvcpphfhzhrzrnnwffnrrhwrhrnrhnrrgfgmmzwmzmhzzsrzrgggnfntnpnqqdhhmrmrrqdqwqsqmssmllmvmgglttdmdffwzzhszhzphzphplpvpzvvsvcvgvqgqpgqqrmqmqssdbdcdpphddvppfggwrgrdgrggtvggrttmpmvmnmwnmmlcccwssfjftfbbgqbbnlldzlddzbbvmmvbmvvbsszggmnggqsqvssdlddqbbtfbttlhhlssplphlhzzcmmdcmmlqlqmmtddztzctzzlglwlzwwsmsddbrrzqqpdpjdpjjzhjjjzttvvwcvcdvvwqvvwqqgpgjgwgvgmmdjjmbbttmffwgwgppspzzvddjnjppmsscjczcqcczlzjzbbzmzllsshrrdtdttjdtdnnwbwttzptztrzzgfgfqfjjlwjjbfbsbmmhphqqjlqjjjshsvvrzrffvnfftrrpzzrdrbbcqbqjqccphhwfwgfwgwgwzzhggddctcsttjgjppghppfhhlnhhhwfwrfrggtjjgjbgbjbrrssjvvlcvvdzvdzzjhhgpprnpnpggfmfgfzfrzzsjjwhhrmmmvppgfffdsfddwlddmccjfcjjrwwsmwsstlsszbbjssjtsjjmpjpqqzffhphfffrzffnbnvnzvnvsvjjbljlflglrlvvghhcrhccnhccdbcdclcbbgffhwhnwwrwbbngnwwlpwlwsllsffzvzbzhbhppvdvfdvffvsvlslcslsqqszqzrrqbrrpjjvgjggbvvpbblnlvnnvrrprbpbvbccrjsdfrnqvhgfrwtvqjfzflnqqgbwlvfwwmrgnsltsqjfcctdwhqrstpvllhfrbnvnhvvgfvhbsgppslqnhmwdnrjnpfmrppppqpmrmcvtndrgwngrblpvrgnbhgtflthdjdtqhwcfzbdwsjshhnglprfcjfdwffmhbvhshbzsgdsbdwpcjfhrccqmjslqjjrwnbtqftqlvgnpmmzlfnmjzvrfslmmvhqwzjqbwsqfcnlmgdwlcbrlqlfwzhcfjsnncnnjqltfccmllhnjczqssjnjmhqbhqzdplbvfdpmdjmgthvqgjzqqschzgtmpdzmvvhlrwjpbqfplcqdbjfjfcrzdclctldvvqphtnvmgzvwprhzmbsbgfjlhvbtcmnccrgrpjlcjqqfcgjhtwvfstrtzszgcprmcngbhbdvjbfvgqdlhzgtzcjqnjmdtmwzchcfhzhgwprgrzbfwbhstfbprhnbzhsglmwhcjbppnshmzlnzsmbhcrmvpdgftfrwjfnmdvtrrqwjmzlbdjppsnvmlstsnrjwslqtqfmfcspzgrqhshhvclvqdfpbmzvsnthmcrzdmzqcjnnmbwbdlbfmcrzjjsrjbwchlvljplqdbjfchbslcvbjvvzfgdzmmnrgqwftppgpfwhfvdqqsrphrqmdtzjghlldfpgnczzjhqhfjvgqmcpzqssfqsfblcvqfttznpmvczprcptgcfwwlwsmqvfrjzcwbhppsjghmltqtcqljmpjzddncbslqdvgzhdvfpdpgpzljrfnjwdtnbdjwzjvmhqvnrdrjmhcfsbjcwtflljwjvtjbsbhzbghnzwtwpwzzwnfhwszsghggqthcbtwjrhdphbdslzmwhpmtjfbnpzspfqrvvtsjpvjmbtwrsqvfzzphllbvmvczsphdtblgdczjsqqthgscdqpvnmbpblspbcmpgjjtjtdrwhrqcgbrvlcsrwzwnjlgbfjbfgwpqpvnnmmgbdrhrwsptmddvtgbjhgzcphzmscjrqlnngpsnmjhvtmnhmqmcwdpjpbjsntcgppqrjndfsfhrhcvgcmrfrrsvnddwjsndlwffrgqnldqnvtgfvdrwtrlcqhltmbvdndzpdqndqrbtzqwbmbtzzsqftjftcnbrtsgvdrmnqlbbmhwmzpngltcslwdnpzpvstpdtfdcqvlwtbppsjvpdbspzlwnfvgcslzvmrpgplbnrvwpfwnrhcncdzptrjsqvghrczmrfwfntqlvlccwtbwqdcngzlqqvvnvfttqmcbqfqndhlsmbvcnstjcbpffmsptdnqbntnhhdctgdbnvwzgwznsgpvmslpmlcffsdtfnlcmbdgbntcslrlhfmrpddmjjttbtcrgbmfsbbwphtmlrdrgffdlnrwndhttjrplstwfwlpnljlcjphdvdvslwvnstqlrsgwdltlqwzdgsltzjqsgwqpbzgvqbdmvqtdgsnhttqprttzrnmddzdnqsgmnhfrmmfnrmltgjqpmgmdzdwpnzdsgstmwlgstmtjtqlhngmpwdqscrjmpmndddppsgthtbmznndswpsftcfltmhbbglnlmlszsssrlgbzcqpngvtgttlblsthmrltgctpgpjmhqqbldfgcrmclsmjhnjspzgwpmwncjgwrgdjmfgrpndztfdssmjhfmstgbjjzvmrpbmfzljpffwgvszwvtclfdnnbswzpjgthcbzpcbmgfvhfwnfthjdmnzdptqflzldnvnmfqnbggsrzjjssdntqhsjltjlfvjhncbrfbhllmqdpdnlbjptdcrqqghvvfvzzvtbvnjhshptcmgcnlmmpgdggdsfnpfsgrbcnfvrvfcqdfdfbgfvqcspvnrrljsgdtsbbcvcqnlwmrfgjhbdnjqgpggnpqtqgwspgljbrsggnmbmdbctwsdzqjmzrcwqgsvnhlpnlnqgfvgdfpgwsbqpqjcjpwwbrtzqhhgswggwnhmzwtvrqcrgbsrjspmczctgprgcwjtnzghvzqgnpmfqdswnzfzdbpdnpcgmwdrpmjtzhhwcnrrpfvqsgbrzngmwdgvjnmcftcjpcmdvcwjgfgvjrblsdnbsgfmjnvvvfwpfhztbgfddlcljzmwrwglnrfbnphlpvvcbfnwpsmsnhshzwrpnljzstmglrwdcctqlbwvqtfpjjdqzgncwgsnhczhvnvnzzmsjhfvgpdrhsmgnrfjcrzblzncdlffjrcqrqqdnjhgwgdgjlgbtphzgrjvmgnqrvdgmtgvpdbzqcvsfctrntjsnfcwnzpslzsvmtjhtcvcdsbqflbmtvclssrvcwjwcbspjgqhznzpttlbsnrpdrnqsddlcvjdrnqcgjhjfdclhwscdpbsjzcmrhgfwdnqbzqjsdqwbqdfcrztzfvclvbnhqstjztqdmsvlfqzmllphgrnwjqpdlrdnsbdltrggvlmdpfdwrdwnsdnscfcjzmrwqpsjwzrqcqcfndlvtfftbhdcbgcgtrfqrqnvwgbpstrtzgpjcjswfcwgvfnwlpprthntmlqbchjcwnqgppchgtvrzjbdzptfqqbmchmpqlnnfvpghjnmshpcjgsprbjpqnpwwddnswnfjvbzwszplhnhgzmzdqncmqrqtbqhdwbtrbrljpcwbdhjzvcqpdgvbtczlhwwzfmgvffnbcglpdqjdsnhhtnvvmtnhlbqcfqjfcgmcnqstzbgmqcfsgrzncwcfrlpsctpspvvdzwtbrhqzhfcwvwqvtzrmfjgpmlsdjmlgwhcldqlhsjvprvnmzcsldzfpzhmzdbqbpwnrsffswbjcwjgblnlcwzqlmcgfstqggdbsqpcpqcgfvn",
     1198,
     3120)>]
-let ``Day6`` (buffer, part1Result, part2Result) =
+let ``Day06`` (buffer, part1Result, part2Result) =
     let packetStart = Day06.part1 buffer
     let messageStart= Day06.part2 buffer
         
@@ -82,7 +82,7 @@ let ``Day6`` (buffer, part1Result, part2Result) =
 [<Theory>]
 [<InlineData("Input/input-day8-test.txt", 21, 8)>]
 [<InlineData("Input/input-day8.txt", 1676, 313200)>]
-let ``Day8`` (fileName, part1Result, part2Result) =
+let ``Day08`` (fileName, part1Result, part2Result) =
     let lines = File.ReadAllLines(fileName)
         
     let sum = Day08.part1 lines
@@ -90,3 +90,15 @@ let ``Day8`` (fileName, part1Result, part2Result) =
         
     Assert.Equal(part1Result, sum)
     Assert.Equal(part2Result, topScenicScore)
+
+[<Theory>]
+[<InlineData("Input/input-day10-test.txt", 13140, 0)>]
+[<InlineData("Input/input-day10.txt", 17380, 0)>]
+let ``Day10`` (fileName, part1Result, part2Result) =
+    let lines = File.ReadAllLines(fileName)
+        
+    let sum = Day10.part1 lines
+    //let topScenicScore = Day10.part2 lines
+        
+    Assert.Equal(part1Result, sum)
+    //Assert.Equal(part2Result, topScenicScore)
