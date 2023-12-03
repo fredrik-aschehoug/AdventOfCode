9825dde5a096ca0e519ef47957c58708c82eb12d
fredrik-aschehoug <15358786+fredrik-aschehoug@users.noreply.github.com>
2023-12-03 20:47:01 +0100

    Day 3 part 1

---
+++ 2023/AdventOfCode2023/Solutions/Day03/Day03.fs
@@ -0,0 +1,54 @@
+﻿module Day03
+
+open System
+open System.Text.RegularExpressions
+open Common.Parsing
+open Common.Casting
+
+let notSymbolRx = Regex(@"\d|\.", RegexOptions.Compiled)
+let numberRx = Regex(@"\d+", RegexOptions.Compiled)
+
+let isSymbol (lookup: char array array) ((x, y): int*int) =
+    not (notSymbolRx.IsMatch(lookup.[x].[y].ToString()))
+
+let isValidCoordinate (lookup: char array array) ((x, y): int*int) =
+    let sampleRow = Array.head lookup
+    let res =
+        match (x,y) with
+        | (x,y) when x < 0 -> false //out of bounds
+        | (x,y) when y < 0 -> false //out of bounds
+        | (x,y) when x >= lookup.Length -> false //out of bounds
+        | (x,y) when y >= sampleRow.Length -> false //out of bounds
+        | (x,y) -> isSymbol lookup (x,y)
+    res
+
+let isPartDigit (lookup: char array array) (x: int) (y: int) =
+    let coordinates = [
+        (x+1,y-1); (x+1,y); (x+1,y+1);
+        (x,y-1);            (x,y+1);
+        (x-1,y-1); (x-1,y); (x-1,y+1)
+    ] // All adjacent coordinates
+    coordinates |> List.exists (isValidCoordinate lookup)
+
+let isPartNumber (lookup: char array array) (x: int) (number: Match) =
+    let columns = [number.Index .. number.Index + (number.Length - 1)]
+    columns |> List.exists (isPartDigit lookup x)
+
+let getValidNumbers (lookup: char array array) (x: int) (line: string) =
+    [for m in numberRx.Matches(line) do yield m]
+    |> List.filter (isPartNumber lookup x)
+    |> List.map(fun m -> m.Value)
+    |> List.map Int32.Parse
+
+let part1 text =
+    let lines = splitNewline text
+    let lookup = lines |> Array.map toCharArray
+
+    lines
+    |> Array.toList
+    |> List.mapi (getValidNumbers lookup)
+    |> List.concat
+    |> List.sum
+
+let part2 text =
+    0
--- 2023/AdventOfCode2023/Tests/Tests.fs
+++ 2023/AdventOfCode2023/Tests/Tests.fs
@@ -27,3 +27,12 @@ let ``Day02`` (fileName, expectedPart1, expectedPart2) =
 
     Assert.Equal(expectedPart1, (Day02.part1 text))
     Assert.Equal(expectedPart2, (Day02.part2 text))
+
+[<Theory>]
+[<InlineData("Day03/Input/test.txt", 4361, 0)>]
+[<InlineData("Day03/Input/prod.txt", 560670, 0)>]
+let ``Day03`` (fileName, expectedPart1, expectedPart2) =
+    let text = File.ReadAllText(fileName)
+
+    Assert.Equal(expectedPart1, (Day03.part1 text))
+    Assert.Equal(expectedPart2, (Day03.part2 text))
