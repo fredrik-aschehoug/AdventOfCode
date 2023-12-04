129fbeac31ce9e62c352e82827e117ac26c79c87
fredrik-aschehoug <15358786+fredrik-aschehoug@users.noreply.github.com>
2023-12-04 17:47:00 +0100

    Day 3 part 2

--- 2023/AdventOfCode2023/Solutions/Day03/Day03.fs
+++ 2023/AdventOfCode2023/Solutions/Day03/Day03.fs
@@ -7,35 +7,42 @@ open Common.Casting
 
 let notSymbolRx = Regex(@"\d|\.", RegexOptions.Compiled)
 let numberRx = Regex(@"\d+", RegexOptions.Compiled)
+let gearRx = Regex(@"\*", RegexOptions.Compiled)
+let digitRx = Regex(@"\d", RegexOptions.Compiled)
+
+type Number = {Value: int; Row: int; ColumnStart: int; ColumnStop: int; }
+
 
 let isSymbol (lookup: char array array) ((x, y): int*int) =
     not (notSymbolRx.IsMatch(lookup.[x].[y].ToString()))
 
 let isValidCoordinate (lookup: char array array) ((x, y): int*int) =
     let sampleRow = Array.head lookup
-    let res =
-        match (x,y) with
-        | (x,y) when x < 0 -> false //out of bounds
-        | (x,y) when y < 0 -> false //out of bounds
-        | (x,y) when x >= lookup.Length -> false //out of bounds
-        | (x,y) when y >= sampleRow.Length -> false //out of bounds
-        | (x,y) -> isSymbol lookup (x,y)
-    res
+    match (x,y) with
+    | (x,y) when x < 0 -> false //out of bounds
+    | (x,y) when y < 0 -> false //out of bounds
+    | (x,y) when x >= lookup.Length -> false //out of bounds
+    | (x,y) when y >= sampleRow.Length -> false //out of bounds
+    | (x,y) -> isSymbol lookup (x,y)
 
-let isPartDigit (lookup: char array array) (x: int) (y: int) =
-    let coordinates = [
+let getAdjacentCoordinates x y = [
         (x+1,y-1); (x+1,y); (x+1,y+1);
         (x,y-1);            (x,y+1);
         (x-1,y-1); (x-1,y); (x-1,y+1)
-    ] // All adjacent coordinates
-    coordinates |> List.exists (isValidCoordinate lookup)
+    ]
+
+let isPartDigit (lookup: char array array) (x: int) (y: int) =
+    getAdjacentCoordinates x y |> List.exists (isValidCoordinate lookup)
 
 let isPartNumber (lookup: char array array) (x: int) (number: Match) =
     let columns = [number.Index .. number.Index + (number.Length - 1)]
     columns |> List.exists (isPartDigit lookup x)
 
-let getValidNumbers (lookup: char array array) (x: int) (line: string) =
+let getNumberInLine line =
     [for m in numberRx.Matches(line) do yield m]
+
+let getValidNumbers (lookup: char array array) (x: int) (line: string) =
+    getNumberInLine line
     |> List.filter (isPartNumber lookup x)
     |> List.map(fun m -> m.Value)
     |> List.map Int32.Parse
@@ -50,5 +57,60 @@ let part1 text =
     |> List.concat
     |> List.sum
 
+let isDigit (lookup: char array array) ((x, y): int*int) =
+    digitRx.IsMatch(lookup.[x].[y].ToString())
+
+let coordinateIsDigit (lookup: char array array) (x,y) = 
+    let sampleRow = Array.head lookup
+    match (x,y) with
+    | (x,y) when x < 0 -> false //out of bounds
+    | (x,y) when y < 0 -> false //out of bounds
+    | (x,y) when x >= lookup.Length -> false //out of bounds
+    | (x,y) when y >= sampleRow.Length -> false //out of bounds
+    | (x,y) -> isDigit lookup (x,y)
+
+let getAdjacentDigits (lookup: char array array) (gear: Match) x =
+    let y = gear.Index
+    getAdjacentCoordinates x y
+    |> List.filter (coordinateIsDigit lookup)
+
+let getNumberFromCoordinates (numbers: Number list) (x,y) =
+    numbers
+    |> List.filter(fun n -> n.Row = x)
+    |> List.filter(fun n -> y >= n.ColumnStart)
+    |> List.filter(fun n -> y <= n.ColumnStop)
+    |> List.head
+
+let calculateRatio (numbers: int list) =
+    (List.head numbers) * (List.last numbers)
+
+let getRatio (lookup: char array array) (numbers: Number list) x (gear: Match) =
+    let partNumbers =
+        getAdjacentDigits lookup gear x
+        |> List.map (getNumberFromCoordinates numbers)
+        |> List.distinct
+        |> List.map(fun x -> x.Value)
+    if List.length partNumbers = 2 then calculateRatio partNumbers else 0
+    
+let getGearRatios (lookup: char array array) (numbers: Number list) (x: int) (line: string) =
+    [for m in gearRx.Matches(line) do yield m]
+    |> List.map (getRatio lookup numbers x)
+    
+let matchToNumber row (numberMatch: Match) = {
+    Value = Int32.Parse numberMatch.Value;
+    Row = row; ColumnStart = numberMatch.Index;
+    ColumnStop = numberMatch.Index + (numberMatch.Length - 1);
+    }
+
+let parseNumberInLine row line =
+    getNumberInLine line |> List.map (matchToNumber row)
+
 let part2 text =
-    0
+    let lines = splitNewline text
+    let lookup = lines |> Array.map toCharArray
+    let numbers = lines |> Array.toList |> List.mapi parseNumberInLine |> List.concat
+    
+    lines |> Array.toList
+    |> List.mapi (getGearRatios lookup numbers)
+    |> List.concat
+    |> List.sum
--- 2023/AdventOfCode2023/Tests/Tests.fs
+++ 2023/AdventOfCode2023/Tests/Tests.fs
@@ -29,8 +29,8 @@ let ``Day02`` (fileName, expectedPart1, expectedPart2) =
     Assert.Equal(expectedPart2, (Day02.part2 text))
 
 [<Theory>]
-[<InlineData("Day03/Input/test.txt", 4361, 0)>]
-[<InlineData("Day03/Input/prod.txt", 560670, 0)>]
+[<InlineData("Day03/Input/test.txt", 4361, 467835)>]
+[<InlineData("Day03/Input/prod.txt", 560670, 91622824)>]
 let ``Day03`` (fileName, expectedPart1, expectedPart2) =
     let text = File.ReadAllText(fileName)
 
