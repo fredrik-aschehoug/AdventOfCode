def4975d3aa1d816327e31e3e592a67a2f2176ec
Fredrik <50200428+fredrik-instech@users.noreply.github.com>
2024-12-08 22:06:28 +0100

    Day 01

---
+++ AdventOfCode2024/Common/Parsing.fs
@@ -0,0 +1,6 @@
+﻿namespace Common
+
+module Parsing =
+    let splitDoubleNewline (text: string) = text.Split("\r\n\r\n")
+    let splitNewline (text: string) = text.Split("\r\n")
+    let splitText (separator: string) (text: string) = text.Split(separator)
---
+++ AdventOfCode2024/Solutions/Day01/Day01.fs
@@ -0,0 +1,30 @@
+﻿module Day01
+
+open Common.Parsing
+open Common.Casting
+
+let getLocationId text part =
+    let parts = splitText "   " text
+    stringToInt parts[part]
+
+let getFirstId text = getLocationId text 0
+let getSecondId text = getLocationId text 1
+
+let getDistance (x: int, y: int) =
+    abs (x - y)
+
+let getSimilarity number rightList =
+    let apperances = rightList |> Array.filter (fun num -> num = number) |> Array.length
+    apperances * number
+
+let part1 text =
+    let lines = text |> splitNewline
+    let first = lines |> Array.map getFirstId |> Array.sort
+    let second = lines |> Array.map getSecondId |> Array.sort
+    Array.zip first second |> Array.map getDistance |> Array.sum
+
+let part2 text =
+    let lines = text |> splitNewline
+    let first = lines |> Array.map getFirstId
+    let second = lines |> Array.map getSecondId
+    first |> Array.map (fun number -> getSimilarity number second) |> Array.sum
